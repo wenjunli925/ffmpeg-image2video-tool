10fb3ca66f03ff9afedd17413f86e160814047a4
Wenjun Li <wenjun@wenjunli.net>
2025-07-28 16:02:53 -0400

    Package ffmpeg in

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
+import os
 import subprocess
-
 import shutil
 
 FFMPEG_BINARY = shutil.which("ffmpeg")
@@ -9,11 +9,31 @@ if not FFMPEG_BINARY:
     FFMPEG_BINARY = "/usr/local/bin/ffmpeg"
 
 
+# def convert_sequence_to_video(input_pattern, output_path, fps=24):
+#     command = [FFMPEG_BINARY, "-framerate", str(fps), "-i", input_pattern, "-c:v", "libx264", "-pix_fmt", "yuv420p", output_path]
+
+#     try:
+#         subprocess.run(command, check=True)
+#         return True, None
+#     except subprocess.CalledProcessError as e:
+#         return False, str(e)
+
+
 def convert_sequence_to_video(input_pattern, output_path, fps=24):
-    command = [FFMPEG_BINARY, "-framerate", str(fps), "-i", input_pattern, "-c:v", "libx264", "-pix_fmt", "yuv420p", output_path]
+    ffmpeg_path = os.path.join(os.path.dirname(__file__), "resources", "ffmpeg")
+    
+    cmd = [
+        ffmpeg_path,
+        "-framerate", str(fps),
+        "-i", input_pattern,
+        "-c:v", "libx264",
+        "-pix_fmt", "yuv420p",
+        "-y",  # Overwrite output if it exists
+        output_path
+    ]
 
     try:
-        subprocess.run(command, check=True)
+        subprocess.run(cmd, check=True)
         return True, None
     except subprocess.CalledProcessError as e:
         return False, str(e)
